2738128b2d32d17416da63bfd4141285face6f8e
Isfanchik16 <111650885+Isfanchik16@users.noreply.github.com>
2024-11-03 16:44:41 +0500

    Update convertor.py

--- convertor.py
+++ convertor.py
@@ -1,6 +1,10 @@
 import unicodedata
+<<<<<<< Updated upstream:convertor.py
 
 def Unicode_Con(char):
+=======
+def Unicde_Con(char):
+>>>>>>> Stashed changes:calculator.py
     unicode_code = f"U+{ord(char):04X}"
     character_name = unicodedata.name(char)
     return (f"The Unicode code point of '{char}' is {unicode_code}\nThe character name of '{char}' is '{character_name}'")
@@ -8,3 +12,11 @@ def Convert(unicode):
     unicode_code = int(unicode[2::],16)
     symbol = chr(unicode_code)
     return (f"The symbol for Unicode U+{unicode_code:04X} is '{symbol}'")
+def encode(name):
+    decimal=[]
+    for x in name:
+        decimal+=[hex(ord(x))[2::]]
+    hexa=" ".join(decimal)
+    return f"Encoded y-name is ({hexa}) in ASCII!"
+print(encode("Husniyor"))
+
