44f7e13a07218f32d3f6488f50f6d23210756d99
Husniyor <husniyor09@gmail.com>
2024-11-03 13:07:08 +0500

    change

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Unicode Converter Project for Telegram Bot
+Before using please put your token in "token.json". 
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from telegram.ext import Updater
-# from telegram import KeyboardButton
+from telegram.ext import Updater, CommandHandler
+from telegram import KeyboardButton
 import json
 
 with open("token.json") as file:
     token = json.load(file)
 
 def start_message(update,context):
-    update.message.reply_text(f"Hello {update.effective_user.name}! To calculate unicode type command /calculate")
+    update.message.reply_text(f"Hello {update.effective_user.name}! To convert unicode type command /convert")
 
 updater = Updater(token)
+dispatcher = updater.dispatcher()
+dispatcher.add_handler(CommandHandler(command="start",callback=start_message))
+updater.start_polling()
+updater.idle()
+
